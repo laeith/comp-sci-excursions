b29a788df2d7297ebf19b652b0562f95d2e31571
laeith <marcin.mazurkiewicz@gmail.com>
2021-12-09 22:06:53 +0100

    Day 7 of AOC2021

---
+++ aoc/adventofcode2021/day7.py
@@ -0,0 +1,34 @@
+import functools
+
+from utils import get_input
+
+
+def part1():
+    sorted_positions = sorted(get_input(7, int, sep=","))
+
+    n = len(sorted_positions)
+
+    # Median position
+    if len(sorted_positions) % 2 == 0:
+        opt_position = sorted_positions[n // 2]
+    else:
+        mid = int(n // 2)
+        opt_position = int((sorted_positions[mid] + sorted_positions[mid + 1]) / 2)
+
+    return sum([abs(i - opt_position) for i in sorted_positions])
+
+
+def part2():
+    data = get_input(7, int, sep=",")
+
+    # Mean position
+    opt_position = int(sum(data) / len(data))
+
+    # Sum for arithmetic sequence
+    def fuel_to_position(start_pos, end_pos):
+        n = abs(end_pos - start_pos)
+        return (n * (1 + n)) // 2
+
+    required_fuel = functools.reduce(lambda prev, curr: prev + fuel_to_position(curr, opt_position), data, 0)
+
+    return required_fuel
