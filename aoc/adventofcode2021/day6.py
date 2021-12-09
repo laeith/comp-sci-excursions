5e383c5ee58f7401532a13904e33f52531855270
laeith <marcin.mazurkiewicz@gmail.com>
2021-12-09 20:53:05 +0100

    Day 6 of AOC2021

--- aoc/adventofcode2021/day6.py
+++ aoc/adventofcode2021/day6.py
@@ -1,6 +1,41 @@
+from collections import deque
+
+from utils import get_input
+
+
+def simulate_lantern_reproduction(simulation_days: int) -> int:
+    """
+    :rtype:int Returns number of lanterns after provided number of days
+    """
+
+    data = get_input(6, int, sep=",")
+
+    days_left = simulation_days
+
+    new_cycle_time = 8
+    old_cycle_time = 6
+
+    reproduction_queue = deque()
+    reproduction_queue.extend([0 for _ in range(new_cycle_time + 1)])
+
+    for lantern_internal_timer in data:
+        reproduction_queue[lantern_internal_timer] = reproduction_queue[lantern_internal_timer] + 1
+
+    while days_left > 0:
+        reproducing_lanterns = reproduction_queue.popleft()
+
+        reproduction_queue.append(reproducing_lanterns)
+        reproduction_queue[old_cycle_time] = reproduction_queue[old_cycle_time] + reproducing_lanterns
+
+        days_left -= 1
+
+    return sum(reproduction_queue)
+
+
 def part1():
-    pass
+    return simulate_lantern_reproduction(80)
 
 
 def part2():
-    pass
+    # Well, doing it in Python has it upsides, doesn't it?
+    return simulate_lantern_reproduction(256)
