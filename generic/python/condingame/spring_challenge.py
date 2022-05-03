2c94438ccb096f8620f1ee776f4144a1af7c99ef
laeith <marcin.mazurkiewicz.ac@gmail.com>
2022-05-03 19:32:20 +0200

    Codingame: Spring Challenge final commit

--- generic/python/condingame/spring_challenge.py
+++ generic/python/condingame/spring_challenge.py
@@ -4,37 +4,34 @@ from enum import Enum
 
 # https://www.codingame.com/ide/challenge/spring-challenge-2022
 
+# The code here is what I did after around ~10h+ of CodinGame Spring Challenge, at the very start it got me fairly high
+# into TOP 70, unfortunately after ~7 days of inactivity the bot got demoted into TOP 800.
+# Yes, it's ugly, and quite bad. Probably acceptable for a first try at this type of challenges.
+
+# A list of things to do:
 # No monster in enemy zone -> Attacker go middle
 # A lot of monsters in enemy, some without shield -> Dive and wind
-# DONT ATTACK MONSTERS IN ENEMY BASE!!!
-# IF MONSTERS within threatening range of enemy base then Wind + Shield if mana > 50
-
 # Improve defenders, probably should take into account enemy attacker position, at least for one of defenders
 # There is an issue where a hero having far higher distance to a monster is assigned to chase it to the base
 #     If no enemy heroes -> simulate if 1 is enough
 #     If enemy heroes -> send both if within our base range
 # Looks like this is the biggest bottleneck at the moment
-# TODO: Marcin: Make the closer defender go to the monster attaking base
-
+# Make the closer defender go to the monster attacking base
+# Farming still not that great
 # Wind -> Shield combo more efficient
 # Control of enemy hero if score guaranteed
-# TODO: Marcin: Don't attack monsters in enemy base
-
-# Better farming
-
-# TODO: Marcin: Control -> Shield for enemies close to base
-
-# TODO: Marcin: We can actually calculate if more than a single defender is needed (but need to take into account enemy)
-
+# Don't attack monsters in enemy base
+# Dive if simulation succeeds
+# Shield for enemies close to base that can be killed but threatened by enemies
+# We can actually calculate if more than a single defender is needed (but need to take into account enemy)
 # At least one defender should prio defense over control
-# TODO: Marcin: Well, now defenders are too aggresive with control
-# TODO: Marcin: Add 'wandering mode' if nothing to do
-# TODO: Marcin: Middle defender sticks too hard initially to middle position
-
-# MORE 'states' e.g. if there are many enemies in enemy base then 'dive mode'
-
-# TODO: Marcin: Simulate if it's better to use wind or just hit to death (rist that enemy will wind it into my base)
-# TODO: Handle fog of war
+# Well, now defenders are too aggresive with control
+# Marcin: Add 'wandering mode' if nothing to do
+# Marcin: Middle defender sticks too hard initially to middle position
+# MORE 'states' e.g. if there are many monsters in enemy base then 'dive mode'
+# Simulate if it's better to use wind or just hit to death (rist that enemy will wind it into my base)
+# Handle fog of war
+# Rework most of it to a more general, intelligent approach
 
 MAX_X = 17_630
 MAX_Y = 9_000
@@ -64,14 +61,14 @@ CONST = {
 }
 
 WEIGHT_MODS = {
-    "BASE_THREATENING_DISTANCE": 6500,
+    "BASE_THREATENING_DISTANCE": 7500,
     "BASE_DANGER_DISTANCE": 4000,
 
     "PRIMARY_DEFENDERS": (1, 4),
     "SECONDARY_DEFENDERS": (2, 5),
     "ATTACKERS": (0, 3),
 
-    "MOVE_THREAT": 50,
+    "MOVE_THREAT": 100,
 
     "DISTANCE_TO_MONSTER_WEIGHT_DIV": 10,
 
@@ -90,6 +87,8 @@ WORLD = {
     "NUM_OF_MON_ATTACKING_ENEMY": 0,
     "NUM_OF_MON_THREATENING_ENEMY": 0,
     "NUM_OF_EN_HEROES_IN_THREATENING_ZONE": 0,
+    "DEFENDERS_IN_ENEMY_BASE": 0,
+    "SEEN_ENEMIES": 0,
 }
 
 HEROES = {}
@@ -104,9 +103,9 @@ class Hero():
 
 class Stance(Enum):
     ATTACK = (transpose((14_000, 6000)), 300)
-    PRIMARY_DEFENSE = (transpose((MAX_X // 2, MAX_Y // 5)), 300)
-    SECONDARY_DEFENSE = (transpose((MAX_X // 5, 2 * MAX_Y // 3)), 300)
-    ROAM_MIDDLE = ((MAX_X // 2, MAX_Y // 2), 200)
+    PRIMARY_DEFENSE = (transpose((MAX_X // 2, MAX_Y // 7)), 250)
+    SECONDARY_DEFENSE = (transpose((MAX_X // 5, 6 * MAX_Y // 7)), 250)
+    ROAM_MIDDLE = (transpose((MAX_X // 2, 6 * MAX_Y // 7)), 200)
     DIVE = (transpose((1450, 1450)), 600)
 
     def __init__(self, position, position_weight):
@@ -165,7 +164,6 @@ def main():
         for hero_id, hero_action_list in actions_per_hero.items():
             hero_action_list.sort(key=lambda item: item[0], reverse=True)
 
-            # if hero_id in WEIGHT_MODS["ATTACKERS"]:
             debug(f"Hero {hero_id} best ({hero_action_list[0][0]}) move: {hero_action_list[0][1]}")
 
             print(hero_action_list[0][1])
@@ -185,10 +183,10 @@ def assign_shield_spells_actions(actions_per_hero, current_mana, hero, monsters)
             # TODO: Marcin: Change it to evaluation i.e. num of enemy heroes, monster health, speed and distance to base
 
             if is_heading_towards_enemy_base(monster) and score < abs(best_spell[0]) \
-                    and distance_to_base < 5000 \
+                    and distance_to_base < 4000 \
                     and monster["shield_life"] < 2 \
                     and monster["health"] > 12:
-                best_spell = (score, create_shield(monster["id"]), monster)
+                best_spell = (9000, create_shield(monster["id"]), monster)
 
     if best_spell[2] is not None:
         best_spell[2]["is_targeted_by_spell"] = True
@@ -206,6 +204,9 @@ def assign_wind_spells_actions(actions_per_hero, mana, hero, monsters):
                 best_spell = (99000, create_wind(WORLD["ENEMY_BASE"]), None)
 
     elif hero.stance == Stance.ATTACK and mana > 30:
+        if WORLD["DEFENDERS_IN_ENEMY_BASE"] == 3:
+            return
+
         for monster in monsters:
             monster_distance_to_en_base = dist(WORLD["ENEMY_BASE"], monster["position"])
             hero_distance_to_en_base = dist(WORLD["ENEMY_BASE"], hero.properties["position"])
@@ -223,12 +224,12 @@ def assign_wind_spells_actions(actions_per_hero, mana, hero, monsters):
 
             if is_heading_towards_enemy_base(monster) \
                     and monster_distance_to_en_base < abs(best_spell[0]) \
-                    and monster_distance_to_en_base < 6000 \
+                    and monster_distance_to_en_base < 7000 \
                     and monster["shield_life"] == 0 \
                     and hero_distance_to_monster < CONST["SPELL_WIND_RANGE"] \
                     and monster["health"] > 14:
                 best_spell = (
-                    monster_distance_to_en_base + 10 * monster["health"], create_wind(WORLD["ENEMY_BASE"]), monster)
+                    monster_distance_to_en_base * 2 + 10 * monster["health"], create_wind(WORLD["ENEMY_BASE"]), monster)
 
     # TODO: Marcin: refactor
     if best_spell[2] is not None:
@@ -242,9 +243,7 @@ def assign_wind_spells_actions(actions_per_hero, mana, hero, monsters):
 
 def assign_control_spell_actions(actions_per_hero, current_mana, hero, monsters):
     best_spell = (-90000, create_wait(), None)
-    # if hero["type"] == HERO_TYPE_DEFENDER and (WORLD["NUM_OF_MON_ATTACKING_US"] > 2 or current_mana > 50):
     if hero.stance in [Stance.PRIMARY_DEFENSE, Stance.SECONDARY_DEFENSE] and current_mana > 80:
-        # Control spell
         for monster in monsters:
             hero_to_monster_dist = dist(monster["position"], hero.properties["position"])
             # debug(
@@ -260,12 +259,27 @@ def assign_control_spell_actions(actions_per_hero, current_mana, hero, monsters)
                     best_spell = (score, create_control(monster["id"], WORLD["ENEMY_BASE"]), monster)
 
     elif hero.stance == Stance.ATTACK:
-        return
+        threatening_monsters = all_monsters_within_distance(monsters, WORLD["ENEMY_BASE"],
+                                                            WEIGHT_MODS["BASE_THREATENING_DISTANCE"])
+        if not threatening_monsters:
+            for monster in monsters:
+                hero_to_monster_dist = dist(monster["position"], hero.properties["position"])
+                if hero_to_monster_dist < 2200 and monster["health"] > 14 and current_mana > 30 \
+                        and not is_heading_towards_enemy_base(monster) \
+                        and monster["near_base"] == 0 \
+                        and not monster["is_targeted_by_spell"] and not monster["is_controlled"]:
+                    score = hero_to_monster_dist + monster["health"]
+
+                    # TODO: Marcin: Doing it for Nth time, time to refactor
+                    if best_spell[0] < score:
+                        best_spell = (score, create_control(monster["id"], WORLD["ENEMY_BASE"]), monster)
+        else:
+            return
 
     if best_spell[2] is not None:
         best_spell[2]["is_targeted_by_spell"] = True
 
-    # Want one defender to prioritize CONTROL
+    # Want one defender to prioritize CONTROL?
     if hero.id in WEIGHT_MODS["PRIMARY_DEFENDERS"]:
         actions_per_hero[hero.id].append((1000 * best_spell[0], best_spell[1], best_spell[2]))
     else:
@@ -299,7 +313,7 @@ def assign_movement_actions(actions_per_hero, hero, monsters):
 
         # TODO: Marcin: This is quite arbitrary...
         # Should probably be a formula of position and health
-        # If not close enough, and not healthy enough...
+        # If not close enough, and not healthy enough, not threatened...
         if best_choice[1] and not (
                 dist(WORLD["BASE"], best_choice[1]["position"]) < CONST["MONSTER_TARGET_RANGE"] and best_choice[1][
             "health"] > 8):
@@ -318,12 +332,21 @@ def assign_movement_actions(actions_per_hero, hero, monsters):
                 monster_hero_dist = dist(hero.properties["position"], monster["position"])
                 monster_default_pos_dist = dist(monster["position"], hero.stance.position)
 
-                monster_hero_score = monster_hero_dist // ((monster_hero_dist / 1400) ** 2)
+                if ((monster_hero_dist / 1400) ** 2) != 0:
+                    monster_hero_score = monster_hero_dist // ((monster_hero_dist / 1400) ** 2)
+                else:
+                    monster_hero_score = 9999  # monster on hero
                 monster_default_pos_score = (monster_default_pos_dist // 90) ** 2
                 score = monster_hero_score - monster_default_pos_score
 
+                threat_score = 0
+                if is_heading_towards_our_base(monster):
+                    threat_score = WEIGHT_MODS["MOVE_THREAT"]
+
                 if is_heading_towards_enemy_base(monster):
                     score = score - 100
+
+                score = score + threat_score
 
                 debug(
                     f"Evalutating attacker for score {score} vs mon: {monster['id']}: {monster_hero_dist} {monster_default_pos_dist} of scores: {monster_hero_score}, {monster_default_pos_score} for monst: {monster}")
@@ -342,26 +365,25 @@ def assign_movement_actions(actions_per_hero, hero, monsters):
             monster_base_dist = dist(WORLD["ENEMY_BASE"], monster["position"])
             monster_default_pos_dist = dist(monster["position"], hero.stance.position)
 
-            # Don't chase too far
-            if monster_default_pos_dist > 5000: continue
             # Don't go after monsters too close to enemy base...
             if monster_base_dist < 1500: continue
+
+            # Don't chase too far
+            if monster_default_pos_dist > 7000 and WORLD["DEFENDERS_IN_ENEMY_BASE"] != 3:
+                continue
 
             monster_hero_score = monster_base_dist // ((monster_base_dist / 2000) ** 2)
             monster_default_pos_score = monster_default_pos_dist // 15
             score = monster_hero_score - monster_default_pos_score
 
-            if is_heading_towards_enemy_base(monster):
-                score = score - 50
+            if is_heading_towards_enemy_base(monster) and monster_base_dist < 4000:
+                continue
 
             debug(
                 f"Evalutating attacker for score {score} vs mon: {monster['id']}: {monster_hero_dist} {monster_base_dist} {monster_default_pos_dist} of scores: {monster_hero_score}, {monster_default_pos_score} for monst: {monster}")
 
             if score >= best_choice[0]:
                 best_choice = (score, monster)
-
-            # if monster_hero_dist < 2200 and 7_000 > monster_base_dist > best_choice[0]:
-            #     best_choice = (monster_base_dist, monster)
 
     else:
         raise RuntimeError(f"Unknown hero type: {hero.properties['type']}")
@@ -380,12 +402,14 @@ def load_current_state():
     monsters = []
     enemies = []
 
-    # Clear mutable data that is supposed to be reconstructed on each turn
+    # Clear mutable data that is supposed to be recalculated on each turn
     WORLD["NUM_OF_MON_ATTACKING_US"] = 0
     WORLD["NUM_OF_MON_ATTACKING_ENEMY"] = 0
     WORLD["NUM_OF_MON_THREATENING_ENEMY"] = 0
     WORLD["NUM_OF_MON_IN_DANGER_ZONE"] = 0
     WORLD["NUM_OF_EN_HEROES_IN_THREATENING_ZONE"] = 0
+    WORLD["DEFENDERS_IN_ENEMY_BASE"] = 0
+    WORLD["SEEN_ENEMIES"] = 0
 
     entity_count = int(input())  # Amount of heroes and monsters you can see
     for i in range(entity_count):
@@ -434,10 +458,13 @@ def load_current_state():
             my_heroes.append(entity)
         if _type == 2:
             enemies.append(entity)
+            WORLD["SEEN_ENEMIES"] += 1
             if entity_my_base_dist < WEIGHT_MODS["BASE_THREATENING_DISTANCE"]:
                 WORLD["NUM_OF_EN_HEROES_IN_THREATENING_ZONE"] += 1
+            if entity_en_base_dist < WEIGHT_MODS["BASE_THREATENING_DISTANCE"]:
+                WORLD["DEFENDERS_IN_ENEMY_BASE"] += 1
 
-    # TODO: Marcin: REFACTOR!
+    # TODO: Marcin: Ekhem...
     for hero_properties in my_heroes:
         hero_id = hero_properties["id"]
         if hero_id in HEROES:
@@ -478,7 +505,7 @@ def debug(msg):
     print(msg, file=sys.stderr, flush=True)
 
 
-def is_within_our_base(position):
+def is_within_threatening_distance(position):
     max_x, max_y = transpose((WEIGHT_MODS["BASE_THREATENING_DISTANCE"], WEIGHT_MODS["BASE_THREATENING_DISTANCE"]))
     if BASE_X == 0:
         return position[0] < max_x and position[1] < max_y
